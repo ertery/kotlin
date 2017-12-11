37a6702fcb690033d54a37b79e23bd4ec18efcd1
SBT-Golubin-AA <golubin.andrew@gmail.com>
2017-12-11 18:09:03 +0300

    add tests

--- src/test/kotlin/br/com/testkotlinboot/pocKotlinBoot/controller/PersonControllerServiceTest.kt
+++ src/test/kotlin/br/com/testkotlinboot/pocKotlinBoot/controller/PersonControllerServiceTest.kt
@@ -4,9 +4,11 @@ package br.com.testkotlinboot.pocKotlinBoot.controller
 import br.com.testkotlinboot.pocKotlinBoot.dto.CardDTO
 import br.com.testkotlinboot.pocKotlinBoot.dto.PersonDTO
 import br.com.testkotlinboot.pocKotlinBoot.dto.PurposeRecord
+import br.com.testkotlinboot.pocKotlinBoot.dto.StatusUpdate
 import br.com.testkotlinboot.pocKotlinBoot.entity.Person
 import br.com.testkotlinboot.pocKotlinBoot.entity.Purpose
 import br.com.testkotlinboot.pocKotlinBoot.entity.PurposePerson
+import br.com.testkotlinboot.pocKotlinBoot.enums.PersonPurposeState
 import br.com.testkotlinboot.pocKotlinBoot.repository.PersonRepository
 import br.com.testkotlinboot.pocKotlinBoot.repository.PurposeRepository
 import br.com.testkotlinboot.pocKotlinBoot.service.PersonControllerService
@@ -71,21 +73,52 @@ internal class PersonControllerServiceTest {
         val pp = PurposePerson(savedPurpose, person)
         person.purposes.add(pp)
 
-        personRepository.saveAndFlush(person)
+       val id = personRepository.saveAndFlush(person).personId
 
-        val purposes = service.findByPersonId(50) as MutableList<PurposeRecord>
+        val purposes = service.findByPersonId(id) as MutableList<PurposeRecord>
         assertEquals(1, purposes.size)
         assertNotNull(purposes[0].persons)
     }
 
     @Test
     fun getPersonByFacebookId() {
-        val person = service.findPersonByFacebookId("1")
+        val personForSave = Person(
+                name = "Test Test",
+                phoneNumber = "8005553535",
+                facebookId = "100",
+                email = "google@goolge.com",
+                registrationDate = LocalDateTime.now()
+        )
+        personRepository.saveAndFlush(personForSave)
+        val person = service.findPersonByFacebookId("100")
         assertNotEquals(-1, person)
     }
 
     @Test
+    @Transactional
     fun updateState() {
+        val savedPurpose = purposeRepository.saveAndFlush(Purpose(
+                name = "Тестовая цель",
+                finishDate = LocalDateTime.of(2018, 10, 23, 17, 15),
+                targetAmmount = 1000.0,
+                imageUrl = "http://testSite.com",
+                description = "Поймай меня, если сможешь"
+        ))
+        val person = Person(
+                name = "Test Test",
+                phoneNumber = "8005553535",
+                facebookId = "1",
+                email = "google@goolge.com",
+                registrationDate = LocalDateTime.now()
+        )
+
+        val pp = PurposePerson(savedPurpose, person)
+        person.purposes.add(pp)
+        val savedPerson =  personRepository.saveAndFlush(person)
+
+        assertEquals(PersonPurposeState.INVITESEND, savedPerson.purposes[0].purposeState)
+        service.updateState(StatusUpdate(personId = savedPerson.personId, purposeId = savedPurpose.purposeId, state = PersonPurposeState.ACCEPT.toString()))
+        assertEquals(PersonPurposeState.ACCEPT, personRepository.findOne(savedPerson.personId).purposes[0].purposeState)
     }
 
     @Test
@@ -117,7 +150,7 @@ internal class PersonControllerServiceTest {
                 id = 0
         ))
         assertEquals(1, personRepository.findAll().size)
-        val savedPerson = service.findPersonByFacebookId("2")
+        val savedPerson = service.findPersonByFacebookId("100")
         assertEquals(-1, savedPerson)
     }
 
@@ -146,7 +179,7 @@ internal class PersonControllerServiceTest {
 
     @After
     fun destroy() {
-
+        personRepository.deleteAll()
     }
 
 
